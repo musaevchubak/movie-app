49281116c61d99ee2e876f37acbc44840c56a68a
Chubak <musaev4t@gmail.com>
2022-10-05 19:28:12 +0600

    Updates

---
+++ precache-manifest.a365a71c060d1f13bf825631c79b0fa7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "356e227c5789b040b469",
+    "url": "/movie-app/static/css/main.23b914c7.chunk.css"
+  },
+  {
+    "revision": "356e227c5789b040b469",
+    "url": "/movie-app/static/js/main.356e227c.chunk.js"
+  },
+  {
+    "revision": "0b981c2a2148a37bcdfd",
+    "url": "/movie-app/static/css/1.6dbc7a3f.chunk.css"
+  },
+  {
+    "revision": "0b981c2a2148a37bcdfd",
+    "url": "/movie-app/static/js/1.0b981c2a.chunk.js"
+  },
+  {
+    "revision": "2a4a27000ba76122af42",
+    "url": "/movie-app/static/js/runtime~main.2a4a2700.js"
+  },
+  {
+    "revision": "29a147e73f2be464e921e375abb55569",
+    "url": "/movie-app/index.html"
+  }
+];
