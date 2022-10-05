736affbc994853bc7c6979ef550bf1ac03d02b7f
Chubak <musaev4t@gmail.com>
2022-10-05 18:40:59 +0600

    Revert "prepared to deploy"
    This reverts commit 8d953cc023aa51e55567d86551d0105af9af583b.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import './styles/App.css';
-import {BrowserRouter} from "react-router-dom";
-import Header from "./components/Header/Header";
-import SimpleBottomNavigation from "./components/MainNav/MainNav";
-import AppRouter from "./components/AppRouter/AppRouter";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-      <BrowserRouter>
-         <Header/>
-         <AppRouter/>
-         <SimpleBottomNavigation/>
-      </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/hooks/useGenre.js
+++
@@ -1,7 +0,0 @@
-const useGeneres = (selectedGenres) => {
-  if (selectedGenres.length < 1) return '';
-  const GenreIds = selectedGenres.map((el) => el.id);
-  return GenreIds.reduce((acc, curr) => acc + "," + curr);
-};
-
-export default useGeneres;
